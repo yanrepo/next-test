eacb92b0d8b7916afdf1a205f5c275c9994f0138
Yan <yanzz@proton.me>
2022-08-17 15:49:04 -0400

    img change

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -1,14 +1,13 @@
 import css from './Card.module.css';
 import Image from 'next/image';
+import img from '../../public/images/food-1920-1280.jpg';
 
 const Card = (i) => {
   return (
     <div className={css.card} key={i.id}>
       <Image
         className={css.cardImg}
-        src={
-          'https://images.unsplash.com/photo-1611671310207-2374ec7b1889?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
-        }
+        src={img}
         layout="intrinsic"
         width={320}
         height={240}
