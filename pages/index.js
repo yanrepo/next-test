9f73cfc844b94f28084723067c2a23bbadde47d4
Yan <yanzz@proton.me>
2022-08-21 10:42:31 -0400

    class="home"

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
+    <div className="home">
       <h1>take a break</h1>
       <Link href={'/food'}>
         <a style={{ color: 'var(--orange)' }}>
           <h1>food</h1>
         </a>
       </Link>
-    </>
+    </div>
   );
 }
