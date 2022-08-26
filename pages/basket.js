9fe40f4e5f620f00366129cff0063026a377bd79
Yan <yanzz@proton.me>
2022-08-26 13:15:21 -0400

    remove Other of food

--- pages/basket.js
+++ pages/basket.js
@@ -1,20 +1,3 @@
-import Card from '../components/card/Card';
-import { loadOther } from '../lib/load-food';
-
-export async function getStaticProps() {
-  const other = await loadOther();
-
-  return { props: { other } };
-}
-
-export default function Basket({ other }) {
-  return (
-    <>
-      <div className="card-box">
-        {other.map((i) => {
-          return <Card key={i.id} title={i.title} text={i.text} />;
-        })}
-      </div>
-    </>
-  );
+export default function Basket() {
+  return <div className="basket"></div>;
 }
