134c4e76b5aead6c90dab0b17be1629ce2bc4b6f
Yan <yanzz@proton.me>
2022-08-26 13:17:08 -0400

    remove link

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div className="home">
       <h1>take a break</h1>
-      <Link href={'/food'}>
-        <a style={{ color: 'var(--orange)' }}>
-          <h1>food</h1>
-        </a>
-      </Link>
     </div>
   );
 }
