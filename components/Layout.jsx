32d3ec402ddedb80008ecaee3317069b6114adee
Yan <yanzz@proton.me>
2022-08-29 05:16:07 -0400

    footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,16 @@
 import SideBar from './navBar/SideBar';
 
+//
+//EMPTY DIV-WRAPPER FOR EMPTY FOOTER
+
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <SideBar />
-      <div className="main">{children}</div>
+      <div>
+        <SideBar />
+        <div className="main">{children}</div>
+        <footer />
+      </div>
     </div>
   );
 };
