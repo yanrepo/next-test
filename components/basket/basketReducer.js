d0575e92552a4027ccc917053132f72967cddcf2
Yan <yanzz@proton.me>
2022-09-13 19:06:28 +0200

    remove console.log

--- components/basket/basketReducer.js
+++ components/basket/basketReducer.js
@@ -7,13 +7,10 @@ const basketReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
     case 'ADD':
-      console.log('ADD', payload);
       return { ...state, products: payload.products };
     case 'REMOVE':
-      console.log('REMOVE', payload);
       return { ...state, products: payload.products };
     case 'TOTAL':
-      console.log('TOTAL', payload);
       return { ...state, total: payload.total };
     default:
       throw new Error(`${type} type not exist.`);
