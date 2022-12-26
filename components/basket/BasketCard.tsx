5d79bc447117810600e1a86cb1f47d670df949e5
yanrepo <yan23mail@gmail.com>
2022-12-27 01:39:14 +0300

    css

--- components/basket/BasketCard.tsx
+++ components/basket/BasketCard.tsx
@@ -2,6 +2,9 @@
 
 import css from './BasketCard.module.css';
 import useBasket from './basketContext';
+import Image from 'next/legacy/image';
+import imgFood from '../../public/images/alexandra-tran-unsplash.png';
+import imgDrinks from '../../public/images/daniel-jankovic-unsplash.png';
 
 const BasketCard = () => {
   const { total, products, removeFromBasket, increament, decreament } =
@@ -19,28 +22,36 @@ const BasketCard = () => {
   return (
     <div className={css.box}>
       <h2 className={css.title}>the total amount: ${total}</h2>
-      <ul className={css.ul}>
-        {products.map((i) => (
-          <li className={css.li} key={i.id}>
-            <button
-              className={css.remove}
-              name={i.id}
-              onClick={(e) => removeFromBasket(e)}
-            >
-              X
-            </button>
-            {`${i.title}: x${i.amount} = $${i.price} `}
-            <div className={css.buttons}>
-              <button name={i.id} onClick={dec}>
-                -
+      {!products[0] ? null : (
+        <ul className={css.ul}>
+          {products.map((i) => (
+            <li className={css.li} key={i.id}>
+              <button
+                className={css.remove}
+                name={i.id}
+                onClick={(e) => removeFromBasket(e)}
+              >
+                X
               </button>
-              <button name={i.id} onClick={inc}>
-                +
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <Image
+                src={i.type === 'food' ? imgFood : imgDrinks}
+                width={30}
+                height={30}
+                alt="image"
+              />
+              {`${i.title}: x${i.amount} = $${i.price} `}
+              <div className={css.buttons}>
+                <button name={i.id} onClick={dec}>
+                  -
+                </button>
+                <button name={i.id} onClick={inc}>
+                  +
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
