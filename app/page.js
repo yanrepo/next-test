77daac17afce07f1c43ae7cbde0d19ba4e77511c
yanrepo <yan23mail@gmail.com>
2023-01-01 03:10:57 +0300

    remove Timer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
-import Timer from '../components/timer/Timer';
 import BasketCard from '../components/basket/BasketCard';
 
 export default function Basket() {
-  return (
-    <>
-      <Timer />
-      <BasketCard />
-    </>
-  );
+  return <BasketCard />;
 }
